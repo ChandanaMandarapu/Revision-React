e0f02b23116d673ebf16190d22c5720def31bfef
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2025-05-09 21:45:50 +0530

    react

---
+++ react-basics/src/App.jsx
@@ -0,0 +1,13 @@
+import Greet from "./components/Greet";
+import Add from "./components/Add"
+function App() {
+  return 
+  <div>
+    <Greet />
+    <Add />
+  </div>
+  
+}
+
+// export default Greet;
+export default Add;
---
+++ react-basics/src/components/Add.jsx
@@ -0,0 +1,5 @@
+const Add = ( )  =>{
+    return <h1>mental maha</h1>;
+}
+
+export default Add;
