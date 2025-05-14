1ffb0eb0da47b92260d1e572032820419277cab8
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2025-05-14 22:30:55 +0530

    css

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
-import './App.css'
-
+import './index.css'
+import './fanta.css'
+import Header from './components/Header'
+import Tabs from './components/Tabs'
+import TodoList from './components/TodoList'
+import TodoInput from './components/TodoInput'
+import TodoCard from './components/TodoCard'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <div>
+      <Header />
+    </div>
   )
 }
 
--- todo-app/src/components/Tabs.jsx
+++ todo-app/src/components/Tabs.jsx
@@ -1,5 +1,3 @@
-
-
 const Tabs = () => {
   return (
     <div>Tabs</div>
